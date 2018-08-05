6303583bcc847df31a62d4abbd47c55751ba34af
Michael Roose <mjroose@gmail.com>
2018-08-05 17:40:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,15 @@
-def reverse_each_word(sentence) #example using .each method
+# def reverse_each_word(sentence) #example using .each method
+#   words = sentence.split(" ")
+#   reversed_words = []
+#   words.each do |word|
+#     reversed_words.push(word.reverse)
+#   end
+#   reversed_words.join(" ")
+# end
+
+def reverse_each_word(sentence) #example using .collect method
   words = sentence.split(" ")
-  reversed_words = []
-  words.each do |word|
-    reversed_words.push(word.reverse)
-  end
-  reversed_words.join(" ")
+  words.collect do |word|
+    word.reverse
+  end.join(" ")
 end
